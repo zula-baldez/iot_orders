1ef69f42d98c85481c87964bf8033d5ad684c640
moyakkauntda <moyakkauntda@gmail.com>
2024-04-30 16:01:58 +0300

    kik

--- product-service-iot/src/main/kotlin/com/iot/productserviceiot/schedule/ExpirationPointTask.kt
+++ product-service-iot/src/main/kotlin/com/iot/productserviceiot/schedule/ExpirationPointTask.kt
@@ -1,7 +1,9 @@
 package com.iot.productserviceiot.schedule
 
 import com.iot.productserviceiot.data.ProductRepository
+import com.iot.productserviceiot.data.ProductSampleRepository
 import com.iot.productserviceiot.data.SelectRequest
+import com.iot.productserviceiot.service.SalesService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.Sort
 import org.springframework.scheduling.annotation.Scheduled
@@ -9,7 +11,9 @@ import org.springframework.stereotype.Component
 
 @Component
 class ExpirationPointsTask(
-    @Autowired private val productRepository: ProductRepository
+    @Autowired private val productRepository: ProductRepository,
+    @Autowired private val sampleRepository: ProductSampleRepository,
+    @Autowired private val salesService: SalesService
 ) {
     @Scheduled(fixedRate = 1000*60*60)
     fun renewProductExpirationPoints() {
@@ -18,7 +22,13 @@ class ExpirationPointsTask(
         val sort = Sort.unsorted()
         var selected = productRepository.findAll(SelectRequest(offset, limit, sort)).content
         while (selected.isNotEmpty()) {
-            selected.forEach { it.expirationPoints -= it.expirationPointsDecreaseSpeed }
+            selected.forEach { it.expirationPoints -= it.expirationPointsDecreaseSpeed; }
+            selected.forEach {
+                val sample = sampleRepository.findAllByTitle(it.title).first()
+                val initialPrice = sample.initialPrice
+                val sale = salesService.getSale(sample.id!!)
+                it.currentPrice  = (sale * 1.0) / 100 * initialPrice
+            }
             productRepository.saveAll(
                 selected
             )
